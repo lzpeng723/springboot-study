b8a7690c17795c0278e98ae939b360017a553b43
lzpeng <1500913306@qq.com>
2018-10-27 17:36:29 +0800

    创建多模块gradle项目

--- sprintboot-web/src/main/java/com/lzpeng/sprintboot/web/WebApplication.java
+++ sprintboot-web/src/main/java/com/lzpeng/sprintboot/web/WebApplication.java
@@ -5,13 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 @SpringBootApplication
 public class WebApplication {
 
 	@GetMapping
 	public String hello(){
-		return "Hello World !";
+		return "Hello World !" + LocalDateTime.now();
 	}
 
 	public static void main(String[] args) {
