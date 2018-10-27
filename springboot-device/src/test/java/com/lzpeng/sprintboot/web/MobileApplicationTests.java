c1001b34b61f56faffc0ed26e3b73585bf4d9201
lzpeng <1500913306@qq.com>
2018-10-27 22:30:27 +0800

    获取客户端信息

--- springboot-device/src/test/java/com/lzpeng/sprintboot/web/MobileApplicationTests.java
+++ springboot-device/src/test/java/com/lzpeng/sprintboot/web/MobileApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WebApplicationTests {
+public class MobileApplicationTests {
 
 	@Test
 	public void contextLoads() {
