07d866d205179a2625d0b44b2b9e21ceb3b61110
lzpeng <1500913306@qq.com>
2018-10-27 22:38:03 +0800

    springboot-web基础使用

---
+++ sprintboot-web/src/test/java/com/lzpeng/sprintboot/web/WebApplicationTest.java
@@ -0,0 +1,17 @@
+package com.lzpeng.sprintboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
