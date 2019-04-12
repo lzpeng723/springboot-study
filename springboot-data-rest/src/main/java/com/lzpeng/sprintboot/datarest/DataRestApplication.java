2f43fd4984029e95849524148fa4fcd9db19be45
lzpeng <1500913306@qq.com>
2019-04-13 00:47:09 +0800

    data-rest简单使用

--- springboot-data-rest/src/main/java/com/lzpeng/sprintboot/datarest/DataRestApplication.java
+++ springboot-data-rest/src/main/java/com/lzpeng/sprintboot/datarest/DataRestApplication.java
@@ -2,11 +2,13 @@ package com.lzpeng.sprintboot.datarest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class DataRestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DataRestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DataRestApplication.class, args);
+    }
 }
